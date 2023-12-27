60a97066c7579232ba0f3f45b7c94aa2caf883be
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-12-27 22:39:59 +0300

    Update vmware.sh

--- vmware.sh
+++ vmware.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-# Function to print colored text
 print_colored() {
     local text="$1"
-    local color="${2:-\e[97m}"  # Default to white
-    local background="${3:-\e[40m}"  # Default to black
+    local color="${2:-\e[97m}"  
+    local background="${3:-\e[40m}"  
     echo -e "${color}${background}${text}\e[0m"
 }
 
-# Function for animated loading
+animate_ui() {
+    for ((i = 1; i <= 3; i++)); do
+        clear
+        print_colored "Loading..." "\e[93m" "\e[1m"
+        sleep 0.5
+        clear
+        print_colored "Welcome!" "\e[96m"
+        sleep 0.5
+    done
+}
+
+animate_crack_operation() {
+    print_colored "Performing additional operations..." "\e[93m" "\e[1m"
+    sleep 2
+    clear
+}
+
+animate_success_message() {
+    for ((i = 1; i <= 3; i++)); do
+        clear
+        print_colored "Success!" "\e[92m" "\e[1m"
+        sleep 0.5
+        clear
+        sleep 0.5
+    done
+}
+
 animate_loading() {
     for ((i = 1; i <= 3; i++)); do
         print_colored "Cracking..." "\e[93m" "\e[1m" -n
@@ -19,66 +44,6 @@ animate_loading() {
     echo
 }
 
-# Function for the main user interface
-show_ui() {
-    clear
-    print_colored "
-    Welcome to the Vmware Cracking script. This script was written by CHEGEBB. Follow me on GitHub for more.
-    ____   ____               _________                       __
-    \\   \\ /   /____           \\_   ___ \\____________    ____ |  | __ ___________
-     \\   Y   /     \\   ______ /    \\  \\/\\_  __ \\__  \\ _/ ___\\|  |/ // __ \\_  __ \\
-      \\     /  Y Y  \\ /_____/ \\     \\____|  | \\/\\  __ \\  \\___|    <\\  ___/|  | \\/
-       \\___/|__|_|  /          \\______  /|__|   \\____/\\___  >__|_ \\\\___  >__|
-                  \\/                  \\/                  \\/     \\/    \\/
-" "\e[96m"
-    print_colored "1. Crack" "\e[92m"
-    print_colored "2. Download" "\e[92m"
-    print_colored "3. Detect Version" "\e[92m"
-    print_colored "4. Exit" "\e[91m"
-
-    read -p "Select an option: " option
-
-    case $option in
-        1) crack_menu ;;
-        2) download ;;
-        3) detect_version ;;
-        4)
-            print_colored "Exiting..." "\e[91m"
-            sleep 1
-            exit ;;
-        *)
-            print_colored "Invalid Option!" "\e[91m"
-            sleep 1
-            show_ui ;;
-    esac
-}
-
-# Function for the crack menu
-crack_menu() {
-    clear
-    print_colored "VMware Cracking Script" "\e[96m"
-    print_colored "1. VMware Workstation Pro" "\e[92m"
-    print_colored "2. VMware Workstation Player" "\e[92m"
-    print_colored "3. VMware Fusion Pro" "\e[92m"
-    print_colored "4. VMware Fusion Player" "\e[92m"
-    print_colored "5. VMware ESXi" "\e[92m"
-
-    read -p "Select an option: " option
-
-    case $option in
-        1) crack "pro" ;;
-        2) crack "player" ;;
-        3) crack "fusion_pro" ;;
-        4) crack "fusion_player" ;;
-        5) detect_version ;;
-        *)
-            print_colored "Invalid Option!" "\e[91m"
-            sleep 1
-            show_ui ;;
-    esac
-}
-
-# Function for animated cracking countdown
 animate_cracking_countdown() {
     for ((i = 20; i >= 0; i--)); do
         print_colored "Cracking in $i seconds..." "\e[93m" "\e[1m"
@@ -87,7 +52,6 @@ animate_cracking_countdown() {
     done
 }
 
-# Function for animated Metasploit-like startup
 animate_startup() {
     frames=(
         " [*] Loading modules..."
@@ -103,13 +67,12 @@ animate_startup() {
     done
 }
 
-# Function for the crack operation
 crack() {
     local version="$1"
     animate_startup
     animate_cracking_countdown
+    animate_crack_operation
 
-    # Keys in the script labeled as valid
     keys=(
         "MC60H-DWHD5-H80U9-6V85M-8280D"
         "4A4RR-813DK-M81A9-4U35H-06KND"
@@ -123,13 +86,12 @@ crack() {
         "0U2J0-2E19P-HJEX1-132Q2-8AKK6"
     )
 
-    # Check if a valid key is present in the script
     local valid_key_found=false
     for key in "${keys[@]}"; do
         if validate_key "$key"; then
             valid_key_found=true
             use_keys "${keys[@]}"
-            print_colored "Exiting..." "\e[91m"
+            animate_success_message
             exit
         fi
     done
@@ -137,13 +99,13 @@ crack() {
     print_colored "Cracking... Please wait." "\e[93m"
     animate_loading
 
-    # Shuffle the keys to select a random one
     keys=($(shuf -e "${keys[@]}"))
 
     for key in "${keys[@]}"; do
         if validate_key "$key"; then
             print_colored "\nCracked Successfully!" "\e[92m"
             open_vmware_with_key_gui "$key"
+            animate_success_message
             break
         fi
     done
@@ -153,28 +115,23 @@ crack() {
     fi
 }
 
-# Function to validate a key
 validate_key() {
     local key="$1"
     # Perform validation here if needed
     return 0  # Placeholder, replace with actual validation
 }
 
-# Function to use keys
 use_keys() {
     local keys=("$@")
-    # Prompt the user to input a key
     print_colored "Using key: ${keys[0]}" "\e[95m"
     read -p "Please open VMware, go to Help > Enter License Key, and input the key. Press Enter when done.: "
 }
 
-# Function to open VMware with a key in GUI
 open_vmware_with_key_gui() {
     local key="$1"
     echo -e "Please input the following key in VMware:\n\n$key"
 }
 
-# Function for downloading
 download() {
     url=""
     if [[ $(uname) == "Darwin" ]]; then
@@ -204,9 +161,7 @@ download() {
     done
 }
 
-# Function to detect the version
 detect_version() {
-    # Check if VMware Workstation is installed by looking for the registry key
     registry_key_path="SOFTWARE\\WOW6432Node\\VMware, Inc.\\VMware Workstation"
     if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
         key_exists=$(reg.exe query "$registry_key_path" 2>/dev/null)
@@ -219,7 +174,6 @@ detect_version() {
         return
     fi
 
-    # Get VMware Workstation version
     version=$(wmic product get name,version | grep "VMware Workstation" | cut -d' ' -f2)
     if [ -n "$version" ]; then
         print_colored "Detected Version: VMware Workstation $version" "\e[92m"
@@ -228,10 +182,68 @@ detect_version() {
     fi
 }
 
-# Main function
+show_ui() {
+    animate_ui
+    clear
+    print_colored "
+    Welcome to the Vmware Cracking script. This script was written by CHEGEBB. Follow me on GitHub for more.
+    ____   ____               _________                       __
+    \\   \\ /   /____           \\_   ___ \\____________    ____ |  | __ ___________
+     \\   Y   /     \\   ______ /    \\  \\/\\_  __ \\__  \\ _/ ___\\|  |/ // __ \\_  __ \\
+      \\     /  Y Y  \\ /_____/ \\     \\____|  | \\/\\  __ \\  \\___|    <\\  ___/|  | \\/
+       \\___/|__|_|  /          \\______  /|__|   \\____/\\___  >__|_ \\\\___  >__|
+                  \\/                  \\/                  \\/     \\/    \\/
+" "\e[96m"
+    animate_ui
+    print_colored "1. Crack" "\e[92m"
+    print_colored "2. Download" "\e[92m"
+    print_colored "3. Detect Version" "\e[92m"
+    print_colored "4. Exit" "\e[91m"
+
+    read -p "Select an option: " option
+
+    case $option in
+        1) crack_menu ;;
+        2) download ;;
+        3) detect_version ;;
+        4)
+            print_colored "Exiting..." "\e[91m"
+            sleep 1
+            exit ;;
+        *)
+            print_colored "Invalid Option!" "\e[91m"
+            sleep 1
+            show_ui ;;
+    esac
+}
+
+crack_menu() {
+    animate_ui
+    clear
+    print_colored "VMware Cracking Script" "\e[96m"
+    print_colored "1. VMware Workstation Pro" "\e[92m"
+    print_colored "2. VMware Workstation Player" "\e[92m"
+    print_colored "3. VMware Fusion Pro" "\e[92m"
+    print_colored "4. VMware Fusion Player" "\e[92m"
+    print_colored "5. VMware ESXi" "\e[92m"
+
+    read -p "Select an option: " option
+
+    case $option in
+        1) crack "pro" ;;
+        2) crack "player" ;;
+        3) crack "fusion_pro" ;;
+        4) crack "fusion_player" ;;
+        5) detect_version ;;
+        *)
+            print_colored "Invalid Option!" "\e[91m"
+            sleep 1
+            show_ui ;;
+    esac
+}
+
 main() {
     show_ui
 }
 
-# Run the main function
 main
