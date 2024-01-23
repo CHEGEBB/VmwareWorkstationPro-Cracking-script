aeb8d7143066e83169133c81f606b482416ce091
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2024-01-23 17:44:02 +0300

    Update vmware.ps1

--- vmware.ps1
+++ vmware.ps1
@@ -18,23 +18,10 @@ function Animate-Loading {
     }
 }
 
-# Function for animated rotating loading
-function Animate-Rotating-Loading {
-    $spinner = @("/", "-", "\", "|")
-    for ($i = 0; $i -le 5; $i++) {
-        foreach ($spin in $spinner) {
-            Write-Host -NoNewline "$spin`r"
-            Start-Sleep -Milliseconds 200
-        }
-    }
-    Clear-Host
-}
-
 # Function to show the main user interface
 function Show-UI {
     Clear-Host
     Print-Colored @"
-    Welcome to the VMware Cracking Script! This script was created by CHEGEBB. Follow me on GitHub:
     ____   ____               _________                       __
     \   \ /   /____           \_   ___ \____________    ____ |  | __ ___________
      \   Y   /     \   ______ /    \  \/\_  __ \__  \ _/ ___\|  |/ // __ \_  __ \
@@ -47,7 +34,6 @@ function Show-UI {
     Print-Colored "2. Download" -ForegroundColor Green
     Print-Colored "3. Detect Version" -ForegroundColor Green
     Print-Colored "4. Exit" -ForegroundColor Red
-    Print-Colored "Written by: CHEGEBB" -ForegroundColor Cyan
 
     $option = Read-Host "Select an option"
 
@@ -83,12 +69,49 @@ function Crack-Menu {
     switch ($option) {
         "1" { Crack "pro" }
         "2" { Crack "player" }
-        "3" { Crack-FusionPro }
+        "3" { Detect-Version }
         "4" { Exit }
+        "5" { ESXi-Menu }
         default {
             Print-Colored "Invalid Option!" -ForegroundColor Red
             Start-Sleep -Seconds 1
             Show-UI
+        }
+    }
+}
+
+# Function for the ESXi menu
+function ESXi-Menu {
+    Clear-Host
+    Print-Colored "ESXi Versions" -ForegroundColor Cyan
+    Print-Colored "1. vSphere 6 Enterprise Plus" -ForegroundColor Green
+    Print-Colored "2. vSphere with Operations Management 6 Enterprise" -ForegroundColor Green
+    Print-Colored "3. vSphere ESXi 7.0 Enterprise Plus" -ForegroundColor Green
+    Print-Colored "4. vSAN 7 Enterprise Plus" -ForegroundColor Green
+    Print-Colored "5. vSphere 7 Enterprise Plus with Add-on for Kubernetes" -ForegroundColor Green
+    Print-Colored "6. vCenter Server 7 Standard" -ForegroundColor Green
+    Print-Colored "7. vSphere ESXi 8.0 Enterprise" -ForegroundColor Green
+    Print-Colored "8. vSphere ESXi 8.0 Enterprise Plus" -ForegroundColor Green
+    Print-Colored "9. vSphere 8.0 Standard" -ForegroundColor Green
+    Print-Colored "10. Back to Main Menu" -ForegroundColor Red
+
+    $option = Read-Host "Select an option"
+
+    switch ($option) {
+        "1" { Crack-ESXi "vSphere 6 Enterprise Plus" }
+        "2" { Crack-ESXi "vSphere with Operations Management 6 Enterprise" }
+        "3" { Crack-ESXi "vSphere ESXi 7.0 Enterprise Plus" }
+        "4" { Crack-ESXi "vSAN 7 Enterprise Plus" }
+        "5" { Crack-ESXi "vSphere 7 Enterprise Plus with Add-on for Kubernetes" }
+        "6" { Crack-ESXi "vCenter Server 7 Standard" }
+        "7" { Crack-ESXi "vSphere ESXi 8.0 Enterprise" }
+        "8" { Crack-ESXi "vSphere ESXi 8.0 Enterprise Plus" }
+        "9" { Crack-ESXi "vSphere 8.0 Standard" }
+        "10" { Show-UI }
+        default {
+            Print-Colored "Invalid Option!" -ForegroundColor Red
+            Start-Sleep -Seconds 1
+            ESXi-Menu
         }
     }
 }
@@ -118,16 +141,6 @@ function Animate-Startup {
     }
 }
 
-# Function for the Fusion Pro crack operation
-function Crack-FusionPro {
-    # Display the Fusion Pro key
-    Print-Colored "Fusion Pro Key: YF390-0HF8P-M81RQ-2DXQE-M2UT6" -ForegroundColor Cyan
-
-    Animate-Cracking-Countdown
-    Print-Colored "Cracked Successfully!" -ForegroundColor Green
-    Open-Vmware-With-Key-Gui "YF390-0HF8P-M81RQ-2DXQE-M2UT6"
-}
-
 # Function for the crack operation
 function Crack {
     param (
@@ -135,6 +148,7 @@ function Crack {
     )
 
     Animate-Startup
+    Animate-Cracking-Countdown
 
     # Keys in the script labeled as valid
     $keys = @(
@@ -159,11 +173,6 @@ function Crack {
         Exit
     }
 
-    # If Fusion Pro is selected, call the specific function
-    if ($version -eq "fusionpro") {
-        Crack-FusionPro
-    }
-
     Print-Colored "Cracking... Please wait." -ForegroundColor Yellow
     Animate-Loading
 
@@ -180,6 +189,72 @@ function Crack {
 
     if (-not $validKeyFound) {
         Print-Colored "No valid key found." -ForegroundColor Red
+    }
+}
+
+
+# Function to crack ESXi
+function Crack-ESXi {
+    param (
+        [string]$version
+    )
+
+    # Use the respective keys based on the selected version
+    $keys = @{
+        "vSphere 6 Enterprise Plus" = @(
+            "1C20K-4Z214-H84U1-T92EP-92838",
+            "1A2JU-DEH12-48460-CT956-AC84D",
+            "MC28R-4L006-484D1-VV8NK-C7R58",
+            "5C6TK-4C39J-48E00-PH0XH-828Q4",
+            "4A4X0-69HE3-M8548-6L1QK-1Y240"
+        )
+        "vSphere with Operations Management 6 Enterprise" = @(
+            "4Y2NU-4Z301-085C8-M18EP-2K8M8",
+            "1Y48R-0EJEK-084R0-GK9XM-23R52",
+            "5Z6HK-61L00-M84A0-VR2GP-26UK4",
+            "MF4M2-4RL8P-085L8-QVANK-8P832",
+            "JY2WK-6G10L-H81Z0-QUCZP-1Y842",
+            "5Z6HK-61L00-M84A0-VR2GP-26UK4"
+        )
+        "vSphere ESXi 7.0 Enterprise Plus" = @(
+            "JJ2WR-25L9P-H71A8-6J20P-C0K3F",
+            "HN2X0-0DH5M-M78Q1-780HH-CN214",
+            "JH09A-2YL84-M7EC8-FL0K2-3N2J2"
+        )
+        "vSAN 7 Enterprise Plus" = @(
+            "JG0RU-8AJEN-M8069-X23X0-330P8"
+        )
+        "vSphere 7 Enterprise Plus with Add-on for Kubernetes" = @(
+            "0G0X0-DL29M-489T1-0C3GK-1Z2J6",
+            "J1608-4GJEQ-071L8-9VA0H-2MUK6",
+            "M10DH-8YL47-474L1-DV3U0-8H054",
+            "MM4MR-2G005-H7039-JVCAP-2RAL8"
+        )
+        "vCenter Server 7 Standard" = @(
+            "MA6NU-6RL07-484V8-WA1XP-87ALF"
+        )
+        "vSphere ESXi 8.0 Enterprise" = @(
+            "4F40H-4ML1K-M89U0-0C2N4-1AKL4"
+        )
+        "vSphere ESXi 8.0 Enterprise Plus" = @(
+            "4V492-44210-48830-931GK-2PRJ4",
+            "HG00K-03H8K-48929-8K1NP-3LUJ4"
+        )
+        "vSphere 8.0 Standard" = @(
+            "4F282-0MLD2-M8869-T89G0-CF240",
+            "0F41K-0MJ4H-M88U1-0C3N0-0A214"
+        )
+    }
+
+    if ($keys.ContainsKey($version)) {
+        $selectedKeys = $keys[$version]
+        Print-Colored "Using keys for $($version):" -ForegroundColor Yellow
+        $selectedKeys | ForEach-Object { Print-Colored $_ -ForegroundColor Yellow }
+        Start-Sleep -Seconds 1
+    } else {
+        Print-Colored "Invalid ESXi version selected!" -ForegroundColor Red
+        Start-Sleep -Seconds 1
+        ESXi-Menu
     }
 }
 
@@ -207,122 +282,67 @@ function Open-Vmware-With-Key-Gui {
     param (
         [string]$key
     )
-
-    # Display the key
-    Print-Colored "VMware Key: $key" -ForegroundColor Cyan
-
+    $objForm = New-Object System.Windows.Forms.Form
+    $objForm.Text = "VMware Key Activation"
+    $objForm.Size = New-Object System.Drawing.Size(300,200)
+    $objForm.StartPosition = "CenterScreen"
+    
+    $objLabel = New-Object System.Windows.Forms.Label
+    $objLabel.Location = New-Object System.Drawing.Size(20,20)
+    $objLabel.Size = New-Object System.Drawing.Size(260,20)
+    $objLabel.Text = "Please input the following key in VMware:`n`n$key"
+    
+    $objForm.Controls.Add($objLabel)
+    
+    $objForm.ShowDialog()
 }
 
-# Function to detect the version with animated progress
-function Detect-Version {
+# Function for downloading
+function Download {
     try {
-        $spinner = @("-", "\\", "|", "/")
-        $versionDetectionMessage = "Detecting Version: VMware Workstation"
-        $timeoutInSeconds = 30
-        $startTime = Get-Date
-        $versionDetermined = $false
-        $progressWidth = 20
-
-        # Start the detection in the background
-        $detectionJob = Start-Job -ScriptBlock {
-            param($versionDetectionMessage, $spinner, $progressWidth)
-            $progress = "[" + ('=' * $progressWidth) + "]"
-            while ($true) {
-                foreach ($spin in $spinner) {
-                    Clear-Host
-                    Print-Colored "$versionDetectionMessage $progress $spin`r" -ForegroundColor Yellow
-                    Start-Sleep -Milliseconds 100
-                }
-            }
-        } -ArgumentList $versionDetectionMessage, $spinner, $progressWidth
-
-        while ((Get-Date).Subtract($startTime).TotalSeconds -lt $timeoutInSeconds -and -not $versionDetermined) {
-            $registryKeyPath = "HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\VMware Workstation"
-            $keyExists = Test-Path $registryKeyPath
-
-            if ($keyExists) {
-                $version = Get-WmiObject Win32_Product | Where-Object { $_.Caption -like "*VMware Workstation*" } | Select-Object -ExpandProperty Version
-
-                if ($version) {
-                    $versionDetermined = $true
-                    Clear-Host
-                    Print-Colored "Detected Version: VMware Workstation $version" -ForegroundColor Green
-                    Stop-Job $detectionJob  # Stop the detection animation
-                    Remove-Job $detectionJob  # Remove the detection job
-                    Start-Sleep -Seconds 2  # Display the version for 2 seconds
-                    Crack-Menu  # Reload the crack menu
-                    return
-                }
-            }
+        $url = if ($IsWindows) {
+            "https://www.vmware.com/go/getworkstation-win"
+        } else {
+            "https://www.vmware.com/go/getworkstation-linux"
         }
 
-        Clear-Host
-        Stop-Job $detectionJob  # Stop the detection animation
-        Remove-Job $detectionJob  # Remove the detection job
-        if (-not $versionDetermined) {
-            Print-Colored "Timed out. Unable to detect the VMware Workstation version." -ForegroundColor Red
-            Start-Sleep -Seconds 2  # Display the error message for 2 seconds
-            Crack-Menu  # Reload the crack menu
+        $response = Invoke-RestMethod -Uri $url -Method Head
+        $downloadUrl = $response.Headers.Location
+
+        if ($IsWindows) {
+            Start-Process $downloadUrl
+        } else {
+            Start-Process xdg-open -ArgumentList $downloadUrl
+        }
+    } catch {
+        Print-Colored "Error!" -ForegroundColor Red
+        Start-Sleep -Seconds 1
+        Download
+    }
+}
+
+# Function to detect the version
+function Detect-Version {
+    try {
+        $registryKeyPath = "HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\VMware Workstation"
+        $keyExists = Test-Path $registryKeyPath
+
+        if (-not $keyExists) {
+            Print-Colored "VMware Workstation is not installed." -ForegroundColor Yellow
+            return
+        }
+
+        $version = Get-WmiObject Win32_Product | Where-Object { $_.Caption -like "*VMware Workstation*" } | Select-Object -ExpandProperty Version
+        if ($version) {
+            Print-Colored "Detected Version: VMware Workstation $version" -ForegroundColor Green
+        } else {
+            Print-Colored "Unable to determine the VMware Workstation version." -ForegroundColor Red
         }
     } catch {
         $errorMessage = $_.Exception.Message
         Print-Colored "Error during version detection: $errorMessage" -ForegroundColor Red
-        Start-Sleep -Seconds 2  # Display the error message for 2 seconds
-        Crack-Menu  # Reload the crack menu
     }
 }
-
-
-# Function to detect the version with rotating animation and timeout
-function Detect-Version {
-    try {
-        $spinner = @("/", "-", "\", "|")
-        $versionDetectionMessage = "Detecting Version: VMware Workstation"
-        $timeoutInSeconds = 30
-        $startTime = Get-Date
-        $versionDetermined = $false
-        $progressWidth = 20
-
-        while ((Get-Date).Subtract($startTime).TotalSeconds -lt $timeoutInSeconds -and -not $versionDetermined) {
-            foreach ($spin in $spinner) {
-                Clear-Host
-                $progress = "[" + ('=' * $progressWidth) + "]"
-                Print-Colored "$versionDetectionMessage $progress $spin`r" -ForegroundColor Yellow
-                Start-Sleep -Milliseconds 200
-            }
-
-            $registryKeyPath = "HKLM:\SOFTWARE\WOW6432Node\VMware, Inc.\VMware Workstation"
-            $keyExists = Test-Path $registryKeyPath
-
-            if ($keyExists) {
-                $version = Get-WmiObject Win32_Product | Where-Object { $_.Caption -like "*VMware Workstation*" } | Select-Object -ExpandProperty Version
-
-                if ($version) {
-                    $versionDetermined = $true
-                    Clear-Host
-                    Print-Colored "Detected Version: VMware Workstation $version" -ForegroundColor Green
-                    Start-Sleep -Seconds 2  # Display the version for 2 seconds
-                    Crack-Menu  # Reload the crack menu
-                    return
-                }
-            }
-        }
-
-        Clear-Host
-        if (-not $versionDetermined) {
-            Print-Colored "Timed out. Unable to detect the VMware Workstation version." -ForegroundColor Red
-            Start-Sleep -Seconds 2  # Display the error message for 2 seconds
-            Crack-Menu  # Reload the crack menu
-        }
-    } catch {
-        $errorMessage = $_.Exception.Message
-        Print-Colored "Error during version detection: $errorMessage" -ForegroundColor Red
-        Start-Sleep -Seconds 2  # Display the error message for 2 seconds
-        Crack-Menu  # Reload the crack menu
-    }
-}
-
-
 
 # Main function
 function Main {
